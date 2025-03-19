d6fcd7bc2ac2c3e4715e724d5d81339e18be40d1
StormBytePP <stormbyte@gmail.com>
2025-03-19 01:13:16 +0100

    Initial commit

---
+++ lib/public/StormByte/network/visibility.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <StormByte/platform.h>
+
+#ifdef WINDOWS
+	#ifdef StormByte_Network_EXPORTS
+		#define STORMBYTE_NETWORK_PUBLIC	__declspec(dllexport)
+  	#else
+      	#define STORMBYTE_NETWORK_PUBLIC	__declspec(dllimport)
+  	#endif
+  	#define STORMBYTE_NETWORK_PRIVATE
+#else
+    #define STORMBYTE_NETWORK_PUBLIC		__attribute__ ((visibility ("default")))
+    #define STORMBYTE_NETWORK_PRIVATE		__attribute__ ((visibility ("hidden")))
+#endif
