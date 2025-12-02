e57adabe96c4baf3495bd4afac4dea123e25e418
David C. Manuelda <StormByte@gmail.com>
2025-12-02 15:09:35 +0100

    Silence warning

--- lib/public/StormByte/network/packet.cxx
+++ lib/public/StormByte/network/packet.cxx
@@ -12,6 +12,6 @@ const unsigned short& Packet::Opcode() const noexcept {
 StormByte::Buffer::FIFO Packet::Serialize() const noexcept {
 	auto opcode_serial = Serializable<unsigned short>(m_opcode);
 	Buffer::FIFO result(opcode_serial.Serialize());
-	result.Write(DoSerialize());
+	(void)result.Write(DoSerialize());
 	return result;
 }
