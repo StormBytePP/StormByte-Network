34596f7590ddd515d4458fa8846a111cc70a6b05
David C. Manuelda <StormByte@gmail.com>
2026-09-04 23:36:55 +0200

    headers: make headers uniform

--- lib/private/StormByte/network/socket/reader.hxx
+++ lib/private/StormByte/network/socket/reader.hxx
@@ -1,21 +1,21 @@
 /*
-* Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
-*
-* This file is part of StormByte-Network.
-*
-* StormByte-Network is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License version 3
-* or later, as published by the Free Software Foundation.
-*
-* StormByte-Network is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public License
-* along with StormByte-Network. If not, see
-* <https://www.gnu.org/licenses/lgpl-3.0.html>.
-*/
+ * Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
+ *
+ * This file is part of StormByte-Network.
+ *
+ * StormByte-Network is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * or later, as published by the Free Software Foundation.
+ *
+ * StormByte-Network is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with StormByte-Network. If not, see
+ * <https://www.gnu.org/licenses/lgpl-3.0.html>.
+ */
 
 #pragma once
 
